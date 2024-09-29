169380948ee01d3ffa7c7a24d6adf94035a3fb26
ganiyat-bakare <bak24006@byui.edu>
2024-09-29 21:12:10 +0100

    home page style and script updated

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,24 +1,9 @@
-// Select the DOM elements for output  
-const currentyear = document.querySelector("#currentyear");  
-const lastModified = document.querySelector("#lastModified");  
+//Modified
+// Get the current year
+const currentYear = new Date().getFullYear();
 
-// Use the Date object  
-const today = new Date();  
+// Update the element with the copyright date
+document.getElementById("currentyear").textContent = ` ${currentYear}`;
 
-// Set the current year  
-currentyear.innerHTML = today.getFullYear();  
-
-// Function to format date and time as "MM/DD/YYYY HH:MM:SS"  
-function formatDate(date) {  
-    const month = String(date.getMonth() + 1).padStart(2, '0');   
-    const day = String(date.getDate()).padStart(2, '0');   
-    const year = date.getFullYear();   
-    const hours = String(date.getHours()).padStart(2, '0');   
-    const minutes = String(date.getMinutes()).padStart(2, '0');   
-    const seconds = String(date.getSeconds()).padStart(2, '0');   
-
-    return `${month}/${day}/${year} ${hours}:${minutes}:${seconds}`;   
-}  
-
-// Set last modified date and time  
-lastModified.innerHTML = `Last Modified: <span id="lastModified">${formatDate(today)}</span>`;
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text
